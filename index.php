32a5477a4851aa88814d05b6e1cfed1b5d9a4339
Phongs4thon <plstr142@gmail.com>
2025-05-06 00:23:25 +0700

    update css

--- index.php
+++ index.php
@@ -40,6 +40,7 @@ $poster_name = [
 
     <!-- main js -->
     <link rel="stylesheet" href="assets/css/style.css">
+    <!-- <link rel="stylesheet" href="assets/css/main.css"> -->
 
     <!-- icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
